93bc27fe6c0ea3add135622b579ab31a848ed049
pohbear <kennethpoh@live.com>
2023-10-12 11:26:57 +0800

    Fixed code

--- ocr-test.py
+++ ocr-test.py
@@ -21,7 +21,8 @@ def remove_noise_gaussian(image):
  
 #thresholding
 def thresholding(image):
-    return cv2.threshold(image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+    return cv2.adaptiveThreshold(image, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 15, 4)
+    # return cv2.threshold(image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
 
 #dilation
 def dilate(image):
@@ -65,12 +66,12 @@ def process_images(image_arr):
     processed_images = [];
     for i, page in enumerate(image_arr):
         # load normal Image as np array
-        page_arr = np.array(page);
-        page_gs = get_grayscale(page_arr);
-        page_gaussian = remove_noise_gaussian(page_gs);
-        page_threshold = thresholding(page_gaussian);
-        processed_images.append(page_threshold);
-    return processed_images;
+        page_arr = np.array(page)
+        page_gs = get_grayscale(page_arr)
+        page_gaussian = remove_noise_gaussian(page_gs)
+        page_threshold = thresholding(page_gaussian)
+        processed_images.append(page_threshold)
+    return processed_images
 
 # pass array of images and create results based on different image processing sequences, write to script folder as png files
 def test_images(image_arr):
@@ -124,14 +125,14 @@ def ocr_find_id(image_arr):
         print(f'Getting data from page {i+1:d}')
         data = pytesseract.image_to_string(page)
         print(f'----------------------data------------------\n{data:s}')
-        if "FME-DO-" in data.upper():
+        if "DELIVERY ORDER" in data.upper():
             do = re.findall(r'FME-DO-\d{4}-[A-Za-z0-9]{8}', data)
             if len(do) != 0:
                 id_name = do[0]
                 break
             else:
                 id_name = "NOT_FOUND_DO_ID_"
-        elif "FME-CS-" in data.upper():
+        elif "CASH SALES TAX INVOICE" in data.upper():
             cs = re.findall(r'FME-CS-\d{4}-[A-Za-z0-9]{8}', data)
             if len(cs) != 0:
                 id_name = cs[0]
@@ -150,14 +151,14 @@ def ocr_find_id(image_arr):
             print(f'Getting data from page {i+1:d}')
             data = pytesseract.image_to_string(page)
             print(f'----------------------data------------------\n{data:s}')
-            if "FME-DO-" in data.upper():
+            if "DELIVERY ORDER" in data.upper():
                 do = re.findall(r'FME-DO-\d{4}-[A-Za-z0-9]{8}', data)
                 if len(do) != 0:
                     id_name = do[0]
                     break
                 else:
                     id_name = "NOT_FOUND_DO_ID_"
-            elif "FME-CS-" in data.upper():
+            elif "CASH SALES TAX INVOICE" in data.upper():
                 cs = re.findall(r'FME-CS-\d{4}-[A-Za-z0-9]{8}', data)
                 if len(cs) != 0:
                     id_name = cs[0]
@@ -182,39 +183,39 @@ scanned_file_arr = []
 filepath_str = 'C:\\Users\\FMEUser15\\Documents\\Repos\\fme-pdf-ocr'
 filepath = os.fsencode(filepath_str)
 
+# # simple navigation through specified encoded filepath to find pdfs
 # for file in os.listdir(filepath):
 #     filename = os.fsdecode(file);
 #     if filename.endswith(".pdf"):
 #         print(os.path.join(filepath_str, filename));
 
-# for subdir, dirs, files in os.walk(filepath):
-#     subdir_str = os.fsdecode(subdir)
-#     for file in files:
-#         filename = os.fsdecode(file)
-#         if filename.endswith('.pdf'):
-#             full_filepath = os.path.join(subdir_str, filename)
-#             # print(full_filepath);
-#             # scanned_file_arr.append(full_filepath);
-#             img = pdf2image.convert_from_path(full_filepath, poppler_path=r'C:\Program Files\poppler-23.08.0\Library\bin')
-#             print(f'----------------------image length------------------\n{len(img):d}')
-#             # print(img)
-#             processed_img = process_images(img)
-#             # print(processed_img)
-#             file_id = ocr_find_id(processed_img)
-#             if "NOT_FOUND" in file_id.upper() or "DELIVERY_NOTE" in file_id.upper():
-#                 file_id = file_id + filename
-#             else:
-#                 file_id = file_id + ".pdf"
-#             print(f'----------------------file id------------------\n{file_id:s}')
-#             # pdf = PdfReader(filename)
-#             new_filepath = subdir_str + "\\" + file_id
-#             os.rename(full_filepath, new_filepath)
+for subdir, dirs, files in os.walk(filepath):
+    subdir_str = os.fsdecode(subdir)
+    for file in files:
+        filename = os.fsdecode(file)
+        if filename.endswith('.pdf'):
+            full_filepath = os.path.join(subdir_str, filename)
+            # print(full_filepath);
+            # scanned_file_arr.append(full_filepath);
+            img = pdf2image.convert_from_path(full_filepath, poppler_path=r'C:\Program Files\poppler-23.08.0\Library\bin')
+            print(f'----------------------image length------------------\n{len(img):d}')
+            # print(img)
+            # print(processed_img)
+            file_id = ocr_find_id(img)
+            if "NOT_FOUND" in file_id.upper() or "DELIVERY_NOTE" in file_id.upper():
+                file_id = file_id + filename
+            else:
+                file_id = file_id + ".pdf"
+            print(f'----------------------file id------------------\n{file_id:s}')
+            # pdf = PdfReader(filename)
+            new_filepath = subdir_str + "\\" + file_id
+            os.rename(full_filepath, new_filepath)
 
 
-img = pdf2image.convert_from_path('C:\\Users\\FMEUser15\\Documents\\Repos\\fme-pdf-ocr\\pdfs\\NOT_FOUND_09102023102055-0011.pdf', poppler_path=r'C:\Program Files\poppler-23.08.0\Library\bin')
-test_images(img)
-file_id = ocr_find_id(img)
-print(file_id)
+# img = pdf2image.convert_from_path('C:\\Users\\FMEUser15\\Documents\\Repos\\fme-pdf-ocr\\pdfs\\NOT_FOUND_09102023102055-0011.pdf', poppler_path=r'C:\Program Files\poppler-23.08.0\Library\bin')
+# test_images(img)
+# file_id = ocr_find_id(img)
+# print(file_id)
 
 # do_imgs = 
 # # print(do_img);
